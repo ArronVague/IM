7438b4a3cb409bc95125cf20b563239c94b56905
SmileExpression <15018135093@163.com>
2024-03-06 00:09:34 +0800

    Create user table

---
+++ app/model/init.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"errors"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"log"
+)
+
+var DbEngine *xorm.Engine
+
+func init() {
+	fmt.Println("init database")
+	driverName := "mysql"
+	dsnName := "root:Wohaijidemima123@(gz-cynosdbmysql-grp-04b3z61j.sql.tencentcdb.com:20464)/chat?charset=utf8"
+	err := errors.New("")
+	DbEngine, err = xorm.NewEngine(driverName, dsnName)
+	if err != nil && err.Error() != "" {
+		log.Fatal(err)
+	}
+	DbEngine.ShowSQL(true)
+	//设置数据库连接数
+	DbEngine.SetMaxOpenConns(10)
+	//自动创建数据库
+	DbEngine.Sync(new(User))
+
+	fmt.Println("init database ok!")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module IM
 go 1.21.5
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/go-xorm/xorm v0.7.9 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	golang.org/x/net v0.22.0 // indirect
+	xorm.io/builder v0.3.6 // indirect
+	xorm.io/core v0.7.2-0.20190928055935-90aeac8d08eb // indirect
 )
